ecde68f6a66ba0205271c34433ce6efe2df97811
root <kogan.gene@gmail.com>
2022-09-16 02:20:47 +0000

    moving more steps

--- setup.py
+++ setup.py
@@ -4,11 +4,12 @@ import sys
 os.environ["TORCH_HOME"] = "/src/.torch"
 os.environ['TRANSFORMERS_CACHE'] = '/src/.huggingface/'
 
+os.system('git clone https://github.com/abraham-ai/stable-diffusion-dev')
 os.system('git clone https://github.com/deforum/k-diffusion')
 os.system('git clone https://github.com/MSFTserver/pytorch3d-lite.git')
-os.system('git clone https://github.com/isl-org/MiDaS.git')
 os.system('git clone https://github.com/genekogan/AdaBins')
-os.system('git clone https://github.com/abraham-ai/stable-diffusion-dev')
+os.system('git clone https://github.com/isl-org/MiDaS.git')
+os.system('mv MiDaS/utils.py MiDaS/midas_utils.py')
 
 sys.path.extend([
     "stable-diffusion-dev",
